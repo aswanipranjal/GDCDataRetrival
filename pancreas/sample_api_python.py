75c94a2b65e579f8750904f2b6c0a3877404c6c0
aswanipranjal <aswani.pranjal@gmail.com>
2017-03-21 00:24:57 +0530

    basic structure is ready, now i just have to process the files and decide on a format to put them into

--- pancreas/sample_api_python.py
+++ pancreas/sample_api_python.py
@@ -2,38 +2,68 @@ import os
 import json
 import requests
 
+headers = {'content-type': 'application/json'}
+
+url = "https://gdc-api.nci.nih.gov/files"
 
 payload = {
-    "filters" : {
-        "op" : "and",
-        "content":[
-            {
-                "op" : "in",
-                "content" : {
-                    "field" : "cases.project.project_id",
-                    "value":[
-                        "TCGA-PAAD"
+"filters":
+        {
+            "op":"and",
+            "content":
+                    [
+                        { 
+                            "op":"in",
+                            "content":
+                                        { 
+                                            "field":"cases.project.project_id", 
+                                            "value":["TCGA-PAAD"]
+                                        } 
+                        },
+
+                        {   "op":"in",
+                            "content":
+                                        {
+                                            "field":"files.access",
+                                            "value":["open"]
+                                        }
+                        },
+
+                        {   "op":"in",
+                            "content":
+                                        {
+                                            "field":"files.data_category",
+                                            "value":["Transcriptome Profiling"]
+                                        }
+                        },
+
+                        {   "op":"in",
+                            "content":
+                                        {
+                                            "field":"files.data_type",
+                                            "value":["Gene Expression Quantification"]
+                                        }
+                        },
+
+                        {   "op":"in",
+                            "content":
+                                        {
+                                            "field":"files.analysis.workflow_type",
+                                            "value":["HTSeq - FPKM"]
+                                        }
+                        }
                     ]
-                }
-            },
-            {
-                "op":"in",
-                "content":{
-                    "field":"files.access",
-                    "value":"open"
-                }
-            }
-        ]
-    }, 
-    "format":"txt",
-    "size":"10"
+        }, 
+            "format":"txt",
+            "size":"10",
+            "facetTab":"files"
 }
 
-headers = {'content-type': 'application/json'}
-url = "https://gdc-api.nci.nih.gov/files"
 r = requests.post(url, json=payload, headers=headers)
 
 data = json.loads(r.text)
 
-os.system("curl --remote-name --remote-header-name 'https://gdc-api.nci.nih.gov/data/{0}'".format(data["data"]["hits"][0]["file_id"]))
-
+file_names = []
+for i in range(len(data["data"]["hits"])):
+    os.system("curl --remote-name --remote-header-name 'https://gdc-api.nci.nih.gov/data/{0}'".format(data["data"]["hits"][i]["file_id"]))
+    file_names.append(data["data"]["hits"][i]["file_name"])
