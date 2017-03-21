3fdc83b974d29ba3434f783c320cbd3abbe23d50
aswanipranjal <aswani.pranjal@gmail.com>
2017-03-21 12:34:30 +0530

    final version, i think

--- pancreas/sample_api_python.py
+++ pancreas/sample_api_python.py
@@ -4,84 +4,51 @@ import requests
 import gzip
 import pandas
 
-from os import listdir
-from os.path import isfile, join
+from functools import reduce
 
+#headers for the request
 headers = {'content-type': 'application/json'}
 
-url = "https://gdc-api.nci.nih.gov/files"
+#url to search the data
+search_url = "https://gdc-api.nci.nih.gov/files"
 
-payload = {
-"filters":
-        {
-            "op":"and",
-            "content":
-                    [
-                        { 
-                            "op":"in",
-                            "content":
-                                        { 
-                                            "field":"cases.project.project_id", 
-                                            "value":["TCGA-PAAD"]
-                                        } 
-                        },
+#url to download the data
+download_url = "https://gdc-api.nci.nih.gov/data"
 
-                        {   "op":"in",
-                            "content":
-                                        {
-                                            "field":"files.access",
-                                            "value":["open"]
-                                        }
-                        },
 
-                        {   "op":"in",
-                            "content":
-                                        {
-                                            "field":"files.data_category",
-                                            "value":["Transcriptome Profiling"]
-                                        }
-                        },
+with open('payload.json') as payload_file:    
+    payload = json.load(payload_file)
 
-                        {   "op":"in",
-                            "content":
-                                        {
-                                            "field":"files.data_type",
-                                            "value":["Gene Expression Quantification"]
-                                        }
-                        },
-
-                        {   "op":"in",
-                            "content":
-                                        {
-                                            "field":"files.analysis.workflow_type",
-                                            "value":["HTSeq - FPKM"]
-                                        }
-                        }
-                    ]
-        }, 
-            "format":"txt",
-            "size":"10",
-            "facetTab":"files"
-}
-
-r = requests.post(url, json=payload, headers=headers)
-
-data = json.loads(r.text)
-
+#making the request
+r = requests.post(search_url, json=payload, headers=headers)
+response_data = json.loads(r.text)
 
 file_names = []
+file_ids = []
 
-for i in range(len(data["data"]["hits"])):
-    os.system("curl --remote-name --remote-header-name 'https://gdc-api.nci.nih.gov/data/{0}'".format(data["data"]["hits"][i]["file_id"]))
-    file_names.append(data["data"]["hits"][i]["file_name"])
+#this loop, downloads the data ans saves the file names and file ids into the lists initialized above
+for i in range(len(response_data["data"]["hits"])):
+    os.system("curl --remote-name --remote-header-name '{0}/{1}'".format(download_url, response_data["data"]["hits"][i]["file_id"]))
+    file_ids.append(response_data["data"]["hits"][i]["file_id"])
+    file_names.append(response_data["data"]["hits"][i]["file_name"])
 
+#get the current working directory
 cwd = os.getcwd()
+dataframe_files = []
 
-with gzip.open('f144de50-6126-4912-9c94-824d1eb0fac5.FPKM.txt.gz', 'rb') as f:
-    file_content1 = pandas.read_table(f, names = ["Gene", "f144de50-6126-4912-9c94-824d1eb0fac5.FPKM"])
+#this loop opens the zip files and converts the data into a pandas.DataFrame object
+for i in range(len(file_names)):
+    with gzip.open(cwd+"/"+file_names[i], 'rb') as f:
+        dataframe_files.append(pandas.read_table(f, names=["Gene", file_ids[i] ]))
 
-with gzip.open('caf9cab4-f98f-46bd-a75d-0eb1e9c6c9ea.FPKM.txt.gz', 'rb') as f:
-    file_content2 = pandas.read_table(f, names = ["Gene", "caf9cab4-f98f-46bd-a75d-0eb1e9c6c9ea.FPKM"])
 
-print(pandas.merge(file_content1, file_content2, on="Gene", how="outer"))
+#merging the dataframes together to form one single data frame
+df_final = reduce(lambda left,right: pandas.merge(left,right,on='Gene'), dataframe_files)
 
+#save the data frame as a TSV file
+df_final.to_csv("response.tsv", sep = " ", index = False)
+
+df = pandas.read_csv("response.tsv", sep=" ")
+
+#print the accumulated data
+print(df)
