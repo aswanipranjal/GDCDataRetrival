074fada50ce651d9f9eea0fb94993601501dedc5
aswanipranjal <aswani.pranjal@gmail.com>
2017-03-30 14:32:06 +0530

    added comments, cleaned up some code

--- sample.py
+++ sample.py
@@ -1,20 +1,29 @@
+"""
+This script uses gdctool utilities created by Broad Institute.
+
+It will download all files belonging to a particular data type in all Project IDs present in a Project.
+All files will be downloaded in a /Data folder.
+
+A folder will be created for each Project ID and inside that folder 10 files will be downloaded for each
+work-flow type specified. The folder will also contain a summary of all the files downloaded in the form
+of TSVs in a text file
+"""
 import os
 import json
 import requests
 import gzip
 import pandas
 import api
-import time
-import sys
 
 from functools import reduce
 
-PROGRAM_NAME = "TCGA"
+PROGRAM_NAME = "TCGA" # Right now we are focusing on TCGA. GDC provides us with TCGA and TARGER
 
-DATA_TYPE = "Gene Expression Quantification"
+DATA_TYPE = "Gene Expression Quantification" # Data type to be downloaded 
 
-WORKFLOW_TYPES = ["HTSeq - FPKM", "HTSeq - FPKM-UQ"]
+WORKFLOW_TYPES = ["HTSeq - FPKM", "HTSeq - FPKM-UQ"] # Work-flow type to be downloaded
 
+# We shall download the data according to the project id.
 PROJECT_ID = ["TCGA-BRCA",
 "TCGA-UCEC",
 "TCGA-KIRC",
@@ -49,20 +58,26 @@ PROJECT_ID = ["TCGA-BRCA",
 "TCGA-DLBC",
 "TCGA-CHOL"]
 
-def create_dir(dir):
-    if not os.path.exists(dir):
-        print("creating directory: " + dir)
-        os.makedirs(dir)
-    else:
-        print(dir + " already exists")
+NUM_FILES = 10
 
+def create_dir(dir_name):
+    if type(dir_name) == type([]):
+        dir_name = "/".join(dir_name)
+    if not os.path.exists(dir_name):
+        print("creating directory: " + dir_name)
+        os.makedirs(dir_name)
+    else:
+        print(dir_name + " already exists.")
 
 def main():
 
     cwd_path = [os.getcwd()]
     cwd_path.append("Data")
-    create_dir("/".join(cwd_path))
+    create_dir(cwd_path)
 
+    # using gdctools' api.py file:
+
+    # adding appropriate filters:
     query_object = api.GDCQuery(endpoint = "files")
     query_object.add_in_filter("file.access", "open")
     query_object.add_in_filter("cases.project.program.name", PROGRAM_NAME)
@@ -70,7 +85,8 @@ def main():
 
     for PI in PROJECT_ID:
         cwd_path.append(PI)
-        create_dir("/".join(cwd_path))
+        #create a folder for the current Project ID
+        create_dir(cwd_path)
         query_object.add_in_filter("cases.project.project_id", PI)
 
         for WFT in WORKFLOW_TYPES:
@@ -79,18 +95,20 @@ def main():
 
             if query_object.hits:
                 cwd_path.append(WFT)
-                create_dir("/".join(cwd_path))
+                #create a folder for the current work-flow type
+                create_dir(cwd_path)
                 dataframe_files = []
 
-                for item in query_object.hits[:10]:
+                for item in query_object.hits[:NUM_FILES]:
                     filename = "/".join(cwd_path) + "/" + item["file_name"]
                     print("Downloading file: "+filename)
                     api.py_download_file(item["file_id"], filename)
 
                     with gzip.open(filename, 'rb') as f:
-                        dataframe_files.append(pandas.read_table(f, names=["Gene", item["file_id"] ]))
+                        dataframe_files.append(pandas.read_table(f, names=["Gene", item["submitter_id"] ]))
                 
                 df_final = reduce(lambda left,right: pandas.merge(left,right,on='Gene'), dataframe_files)
+                #create a condensed TSV file having all the downloaded samples
                 df_final.to_csv("/".join(cwd_path)+".txt", sep = " ", index = False)
                 cwd_path.pop()
             query_object._filters.pop()
